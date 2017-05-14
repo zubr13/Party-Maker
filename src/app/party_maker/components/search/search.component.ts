5d44442075d9e8cf7e43bbe68b48ead89c2738af
Ivan Prodaiko <prodaiko94@gmail.com>
2017-05-14 15:37:51 +0300

    small fixes;

--- src/app/party_maker/components/search/search.component.ts
+++ src/app/party_maker/components/search/search.component.ts
@@ -121,20 +121,16 @@ export class SearchComponent implements OnInit {
                 this.center.latitude, event.latitude) < this.options.radius);
   }
 
-  rad (x) {
-    return x * Math.PI / 180;
-  }
-
   getDistance (lon1, lon2, lat1, lat2) {
+    const rad = x =>  x * Math.PI / 180;
     const R = 6378137;
-    const dLat = this.rad(lat2 - lat1);
-    const dLong = this.rad(lon2 - lon1);
+    const dLat = rad(lat2 - lat1);
+    const dLong = rad(lon2 - lon1);
     const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.rad(lat1)) * Math.cos(this.rad(lat2)) *
+      Math.cos(rad(lat1)) * Math.cos(rad(lat2)) *
       Math.sin(dLong / 2) * Math.sin(dLong / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * c;
-    return d;
+    return R * c;
   }
 
   goToEvent (event) {
