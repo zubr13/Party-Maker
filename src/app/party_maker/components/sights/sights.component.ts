a7690396c966f4d13a81587ca86b32fa9370a6dc
Ivan Prodaiko <prodaiko94@gmail.com>
2017-05-14 08:11:00 +0300

    parsed data from wiki

--- src/app/party_maker/components/sights/sights.component.ts
+++ src/app/party_maker/components/sights/sights.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Http } from '@angular/http';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sights',
@@ -9,18 +10,33 @@ import { Headers, Http } from '@angular/http';
 export class SightsComponent implements OnInit {
   lat: any;
   lng: any;
-
-  constructor(private http: Http) { }
+  private events: Array<Object>;
+  constructor(
+      private http: Http,
+      private router: Router
+  ) { }
 
   ngOnInit() {
     navigator.geolocation.getCurrentPosition(position => {
         this.lat  = position.coords.latitude;
         this.lng = position.coords.longitude;
         this.http.get('http://en.wikipedia.org/w/api.php?action=query&format=json&prop=coordinates%7Cpageimages%7Cpageterms%7Ccategories&generator=geosearch&colimit=50&piprop=thumbnail&pithumbsize=144&pilimit=50&wbptterms=description&clcategories=&ggscoord=50.44728800%7C30.42298000&ggsradius=10000&ggslimit=50&origin=*')
-        .subscribe((data) => {
-          console.log(data.json());
-        });
+            .map(data => data.json().query.pages)
+            .map(pages => Object.keys(pages).map(k => pages[k]))
+            .map(sights => sights.map(sight => {
+                const {lat, lon} = sight.coordinates[0];
+                return Object.assign(sight, { latitude: lat, longtitude: lon })
+            }))
+            .subscribe(data => {
+                this.events = data;
+                console.log(this.events);
+            });
     });
   }
+
+    goToEvent (event) {
+        this.router.navigate(['/app', 'event', event.index]);
+    }
+
 
 }
